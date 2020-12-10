5c85faeaf82774c7cc8ea1195029ba884d359f85
Maciej <maciej.zelazo@protonmail.com>
2020-12-10 20:07:16 +0100

    changed navigation in mobile

--- dist/app.c328ef1a.js
+++ dist/app.c328ef1a.js
@@ -1337,7 +1337,7 @@ $(document).ready(function () {
     $('.nav-toggle').toggleClass('nav-toggle--entered');
   });
 });
-},{"wow.js":"node_modules/wow.js/dist/wow.js","stickybits":"node_modules/stickybits/dist/stickybits.es.js"}],"../../../../AppData/Roaming/npm-cache/_npx/8828/node_modules/parcel/src/builtins/hmr-runtime.js":[function(require,module,exports) {
+},{"wow.js":"node_modules/wow.js/dist/wow.js","stickybits":"node_modules/stickybits/dist/stickybits.es.js"}],"../../../../AppData/Roaming/npm-cache/_npx/18688/node_modules/parcel/src/builtins/hmr-runtime.js":[function(require,module,exports) {
 var global = arguments[3];
 var OVERLAY_ID = '__parcel__error__overlay__';
 var OldModule = module.bundle.Module;
@@ -1365,7 +1365,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "60680" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "52534" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
@@ -1541,5 +1541,5 @@ function hmrAcceptRun(bundle, id) {
     return true;
   }
 }
-},{}]},{},["../../../../AppData/Roaming/npm-cache/_npx/8828/node_modules/parcel/src/builtins/hmr-runtime.js","app.js"], null)
+},{}]},{},["../../../../AppData/Roaming/npm-cache/_npx/18688/node_modules/parcel/src/builtins/hmr-runtime.js","app.js"], null)
 //# sourceMappingURL=/app.c328ef1a.js.map
